7cc7a50f0331a77904c152a4b9529db93bd78690
CodeSmitty <varcodesmitty@gmail.com>
2022-10-25 22:04:35 -0400

    edit, update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,4 @@
+
 class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
@@ -11,6 +12,10 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
         if @article.save
@@ -21,5 +26,18 @@ class ArticlesController < ApplicationController
         end
 
     end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated Succesfully."
+            redirect_to @article
+        else
+            render 'edit'
+        
+    end
+    end
+
+
 
 end 
